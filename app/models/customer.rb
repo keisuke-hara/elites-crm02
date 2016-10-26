6f0a00c5932d9c67df1b3e5b4f7c4e4f5ae884cf
keisuke <hara-keisuke@av.avex.co.jp>
2016-10-26 10:08:57 +0000

    03_実習_companyモデルの追加とcustomerモデルとのアソシエーション完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,7 +10,7 @@ class Customer < ActiveRecord::Base
     validates :email,
         presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
     
-    
+    validates :company_id, presence: true
     
     def full_name
     family_name + given_name
