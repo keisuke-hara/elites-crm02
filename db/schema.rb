0244cad0df7a291f3a94cf10d8f8b33be094a78d
keisuke <hara-keisuke@av.avex.co.jp>
2016-10-26 04:35:47 +0000

    トラブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20161026033032) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
-    t.string   "ulr"
+    t.string   "url"
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
