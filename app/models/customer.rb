2715849b1f8087112ae634d2756363c8bf39bb3e
keisuke <hara-keisuke@av.avex.co.jp>
2016-10-27 06:55:56 +0000

    04_実習_postモデルの追加とcustomerモデルとのアソシエーション.完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ActiveRecord::Base
     
     belongs_to :company
+    belongs_to :post
+    
     
     validates :family_name,
         presence: true, length: { maximum: 20 }
@@ -10,7 +12,8 @@ class Customer < ActiveRecord::Base
     validates :email,
         presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
     
-    validates :company_id, presence: true
+    validates :company_id,  presence: true
+    validates :post_id,     presence: true
     
     def full_name
     family_name + given_name
