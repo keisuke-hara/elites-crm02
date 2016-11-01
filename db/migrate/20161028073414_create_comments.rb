6c0e4bbfcc1a4c83f1094de6808d2df4ae5f119c
keisuke <hara-keisuke@av.avex.co.jp>
2016-11-01 12:00:24 +0000

    08_基本課題.完了

--- db/migrate/20161028073414_create_comments.rb
+++ db/migrate/20161028073414_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :body
+      t.text :body, length: { maximum: 200 }
       t.references :customer, index: true, foreign_key: true
 
       t.timestamps null: false
